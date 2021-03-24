23f1e57c450cbcfc173a0a6ca698d98d16e04c6e
DemyCode <mehdi.bekhtaoui@epita.fr>
2021-03-24 21:22:29 +0100

    [FEAT] Now can create Inverters

--- app.js
+++ app.js
@@ -1,31 +1,64 @@
-addinputnodes = function() {this.inputnodes.push('IN');}
-addoutputnodes = function() {this.outputnodes.push('OUT');}
-removeinputnodes = function() {this.inputnodes.pop();}
-removeoutputnodes = function() {this.outputnodes.pop();}
+function addinteractor() {this.inputnodes.push('IN');}
+function removeinteractor() {this.inputnodes.pop();}
+function addoutput() {this.outputnodes.push('OUT');}
+function removeoutput() {this.outputnodes.pop();}
 
-function removeinputnodes()
+var draggedelement;
+var inverterid = 0;
+
+function addinverter()
 {
-    this.inputnodes.pop()
+  this.inverters.push('inverter_' + inverterid);
+  inverterid += 1;
+}
+
+function mover(event)
+{
+  if (draggedelement)
+  {
+    let w = parseInt(getComputedStyle(draggedelement).width);
+    let h = parseInt(getComputedStyle(draggedelement).height);
+    draggedelement.style.left = (event.clientX - w / 2) + "px";
+    draggedelement.style.top = (event.clientY - h / 2) + "px";
+  }
+}
+
+function dragged(event)
+{
+  draggedelement = event.target;
+}
+
+function released(event)
+{
+  draggedelement = null;
+  console.log('releaser')
 }
 
 const vm = Vue.createApp({
   data() {
     return{
-      inputnodes: ['IN'],
-      outputnodes: ['OUT']
+      inputnodes: [],
+      outputnodes: [],
+      inverters : [],
+      linkers : []
     }
   },
   methods:{
-    addinputnodes,
-    removeinputnodes,
-    addoutputnodes,
-    removeoutputnodes
+    addinteractor,
+    removeinteractor,
+    addoutput,
+    removeoutput,
+    addinverter,
+    dragged,
   }
 })
 
-vm.component('note', {
-  props : ['content'],
-  template : `<p> {{ content }} </p>`
-})
+// vm.component('inverter', {
+//   props : ['content'],
+//   template : `<div class="inverter" @mousedown="mousedown" @mouseup="mouseup"></div>`
+// })
 
 vm.mount('#app')
+
+window.addEventListener('mousemove', mover)
+window.addEventListener('mouseup', released)
